f12eb47b77513268fc383925ee2b51bc3bb0d71b
sseelam2 <sseelam2@ncsu.edu>
2019-11-14 19:53:12 -0500

    Staff and vitals

---
+++ src/main/java/com/models/Vitals.java
@@ -0,0 +1,30 @@
+package com.models;
+
+public class Vitals {
+	
+	private String systolic;
+	private String diastolic;
+	private float temperature;
+	
+	public String getSystolic() {
+		return systolic;
+	}
+	public void setSystolic(String systolic) {
+		this.systolic = systolic;
+	}
+	public String getDiastolic() {
+		return diastolic;
+	}
+	public void setDiastolic(String diastolic) {
+		this.diastolic = diastolic;
+	}
+	public float getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+
+	
+	
+}
